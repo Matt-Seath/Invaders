639dc2733be45c203816c336e53ee58f047feb0e
Matt-Seath <100132940+Matt-Seath@users.noreply.github.com>
2022-06-28 21:30:54 +1000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+#![allow(unused)]
+
+use bevy::prelude::*;
+use components::{Movable, Player, Velocity};
+use player::PlayerPlugin;
+
+mod components;
+mod player;
+
+// Asset constants
+const PLAYER_SPRITE: &str = "player_a.png";
+const PLAYER_SIZE: (f32, f32) = (144., 75.);
+const PLAYER_LASER_SPRITE: &str = "laser_a.png";
+const PLAYER_LASER_SIZE: (f32, f32) = (9., 54.);
+const SPRITE_SCALE: f32 = 0.5;
+
+// Game constants
+const TIME_STEP: f32 = 1. / 60.;
+const BASE_SPEED: f32 = 500.;
+
+// Resources
+pub struct WinSize {
+    pub w: f32,
+    pub h: f32,
+}
+
+struct GameTextures {
+    player: Handle<Image>,
+    player_laser: Handle<Image>,
+}
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
+        .insert_resource(WindowDescriptor {
+            title: "Invaders".to_string(),
+            width: 600.0,
+            height: 600.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
+        .add_startup_system(setup_system)
+        .add_system(movable_system)
+        .run();
+}
+
+fn setup_system(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut windows: ResMut<Windows>,
+) {
+    // Camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // Capture window size
+    let window = windows.get_primary_mut().unwrap();
+    let (win_w, win_h) = (window.width(), window.height());
+
+    // Add WinSize resource
+    let win_size = WinSize { w: win_w, h: win_h };
+    commands.insert_resource(win_size);
+
+    // add GameTextures resource
+    let game_textures = GameTextures {
+        player: asset_server.load(PLAYER_SPRITE),
+        player_laser: asset_server.load(PLAYER_LASER_SPRITE),
+    };
+    commands.insert_resource(game_textures);
+}
+
+fn movable_system(
+    mut commands: Commands,
+    win_size: Res<WinSize>,
+    mut query: Query<(Entity, &Velocity, &mut Transform, &Movable)>,
+) {
+    for (entity, velocity, mut transform, movable) in query.iter_mut() {
+        let translation = &mut transform.translation;
+        translation.x += velocity.x * TIME_STEP * BASE_SPEED;
+        translation.y += velocity.y * TIME_STEP * BASE_SPEED;
+
+        if movable.auto_despawn {
+            // Despawn when out of screen
+            const MARGIN: f32 = 200.;
+            if translation.y > win_size.h / 2. + MARGIN
+                || translation.y < -win_size.h / 2. - MARGIN
+                || translation.x > win_size.w / 2. + MARGIN
+                || translation.x < -win_size.w / 2. - MARGIN
+            {
+                commands.entity(entity).despawn();
+            }
+        }
+    }
+}
